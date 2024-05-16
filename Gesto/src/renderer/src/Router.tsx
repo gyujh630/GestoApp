715bbc5c443421020d4815cad6496b0671baa1cb
tmddv <105033488+tmddv@users.noreply.github.com>
2024-05-17 03:01:25 +0900

    fix: 라우터 path 추가

--- Gesto/src/renderer/src/Router.tsx
+++ Gesto/src/renderer/src/Router.tsx
@@ -2,6 +2,9 @@ import App from "@renderer/App";
 import RenderPdf from "@renderer/pages/RenderPdf";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Presentation from "./pages/Presentation";
+import PreparePresentation from "./pages/PreparePresentation";
+import RecognizeGesture from "./pages/RecognizeGesture";
 
 const Router = () => {
     return (
@@ -12,6 +15,9 @@ const Router = () => {
           <Routes>
             <Route path="/" element={<App/>} />
             <Route path="/RenderPdf" element={<RenderPdf/>}/>
+            <Route path="/Presentation" element={<Presentation/>}/>
+            <Route path="/PreparePresentation" element={<PreparePresentation/>}/>
+            <Route path="/RecognizeGesture" element={<RecognizeGesture/>}/>
           </Routes>
           </div>
       </div>
